bedd0876c4456b3ce4d85bb97c6582fa1bd266b2
Thomas Flaherty <tom.flaherty03@gmail.com>
2026-08-02 14:15:20 +0100

    update liquid-assets-inflate-proc-macro to liquid-assets-inflate-macros

--- liquid-assets-inflate/src/lib.rs
+++ liquid-assets-inflate/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-pub use liquid_assets_inflate_proc_macro::include_assets;
+pub use liquid_assets_inflate_macros::include_assets;
 
 /// The Decompressor trait should be implemented on a struct and can be passed
 /// to assets to decompress into a buffer
